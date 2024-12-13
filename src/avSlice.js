f4c55e42c5331344bbfa23c7a94ce5f5e25ac4d9
aakansha <aakuchindarkar@gmail.com>
2024-12-13 15:34:39 +0530

    Commtiing  image changes Changes

--- src/avSlice.js
+++ src/avSlice.js
@@ -4,36 +4,23 @@ export const avSlice = createSlice({
   name: "av",
   initialState: [
         {
-        img: "",
+        img: "https://media.istockphoto.com/id/652812364/photo/portable-working-beamer-projector.jpg?s=1024x1024&w=is&k=20&c=ljlgBfu_H6GQiJrwRrkgdGSfK3Vr1lj3HvgsA5UcK4g=",
         name: "Projectors",
         cost: 200,
         quantity: 0,
     },
     {
-        img: "",
+        img: "https://pixabay.com/photos/techland-audio-audiovector-stereo-1822627/",
         name: "Speaker",
         cost: 35,
         quantity: 0,
     },
     {
-        img: "",
+        img: "https://www.istockphoto.com/photo/retro-vintage-microphone-on-yellow-background-studio-gm924425774-253700135?utm_source=pixabay&utm_medium=affiliate&utm_campaign=SRP_photo_sponsored&utm_content=https%3A%2F%2Fpixabay.com%2Fphotos%2Fsearch%2Fmicrophones%2F&utm_term=microphones",
         name: "Microphones",
         cost: 45,
         quantity: 0,
-    },
-    {
-        img: "",
-        name: "Whiteboards",
-        cost: 80,
-        quantity: 0,
-    },
-
-    {
-        img: "",
-        name: "Signage",
-        cost: 80,
-        quantity: 0,
-    },
+    }
 
   ],
 
--- src/venueSlice.js
+++ src/venueSlice.js
@@ -5,35 +5,29 @@ export const venueSlice = createSlice({
   name: "venue",
   initialState: [
     {
-      img: "",
+      img: "https://www.istockphoto.com/photo/boardroom-with-blank-screen-gm617867580-107366431?utm_source=pixabay&utm_medium=affiliate&utm_campaign=SRP_photo_sponsored&utm_content=https%3A%2F%2Fpixabay.com%2Fphotos%2Fsearch%2Fconference%2520room%2F&utm_term=conference+room",
       name: "Conference Room (Capacity:15)",
       cost: 3500,
       quantity: 0,
     },
     {
-      img: "",
+      img: "https://cdn.pixabay.com/photo/2013/02/26/01/10/auditorium-86197_1280.jpg",
       name: "Auditorium Hall (Capacity:200)",
       cost: 5500,
       quantity: 0,
     },
     {
-      img: "",
+      img: "https://cdn.pixabay.com/photo/2015/04/20/06/43/meeting-room-730679_1280.jpg",
       name: "Presentation Room (Capacity:50)",
       cost: 700,
       quantity: 0,
     },
     {
-      img: "",
+      img: "https://cdn.pixabay.com/photo/2017/08/23/16/03/iocenters-2673316_640.jpg",
       name: "Large Meeting Room (Capacity:10)",
       cost: 900,
       quantity: 0,
-    },
-    {
-      img: "",
-      name: "Small Meeting Room (Capacity:5)",
-      cost: 1100,
-      quantity: 0,
-    },
+    }
   
   ],
   reducers: {
